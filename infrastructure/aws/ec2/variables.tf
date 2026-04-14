d7fa6ccf4b68b772bd2566bcabc96577a1ece2ac
Jason Shen <jason_2000_nz@hotmail.com>
2026-04-15 01:00:53 +1200

    added ec2 deployment to aws

---
+++ infrastructure/aws/ec2/variables.tf
@@ -0,0 +1,22 @@
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type (t3.micro is free-tier eligible)"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "key_pair_name" {
+  description = "Name of an existing EC2 key pair for SSH access"
+  type        = string
+}
+
+variable "ssh_allowed_cidrs" {
+  description = "CIDR blocks allowed to SSH into the instance"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
